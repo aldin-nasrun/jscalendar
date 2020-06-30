27ba07a21b4065c2dbaad7e4717e41cf6bf7759b
aldin-nasrun <adnzain40@gmail.com>
2020-06-30 22:01:27 +0700

    complete css, add clock feature

--- script/script.js
+++ script/script.js
@@ -1,15 +1,22 @@
 const lang = navigator.language;
-let date = new Date();
-
-let month = date.getMonth();
-let num = date.getDate();
-let day = date.toLocaleString(lang, { weekday: "long" });
-let monthName = date.toLocaleString(lang, { month: "long" });
-let year = date.getFullYear();
-
-document.querySelector("#month").innerHTML = monthName;
-document.querySelector("#day").innerHTML = day;
-document.querySelector("#num").innerHTML = num;
-document.querySelector("#year").innerHTML = year;
-
+function setDate() {
+  let date = new Date();
+  // calendar
+  let month = date.getMonth();
+  let num = date.getDate();
+  let day = date.toLocaleString(lang, { weekday: "long" });
+  let monthName = date.toLocaleString(lang, { month: "long" });
+  let year = date.getFullYear();
+  // clock
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+  document.querySelector("#month").innerHTML = monthName;
+  document.querySelector("#day").innerHTML = day;
+  document.querySelector("#num").innerHTML = num;
+  document.querySelector("#year").innerHTML = year;
+  document.querySelector("#h").innerHTML = `${hour} : ${minute} : ${second}`;
+}
+setInterval(setDate, 1000);
+setDate();
 // document.getElementById("month").innerHTML = monthName;
